aa131a60255090ccf386771118bbee9ced1c5f7c
Erika Redmark <maniacmonkeyinspace@gmail.com>
2014-06-05 20:47:19 -0700

    Commit of uncommited file

---
+++ Monkey Shines/src/edu/nova/erikaredmark/monkeyshines/GameUIController.java
@@ -0,0 +1,17 @@
+package edu.nova.erikaredmark.monkeyshines;
+
+/**
+ * 
+ * Acts as a backing controller to the main UI panel on the main game window. The
+ * UI draws, the World contains stateful data that is reflected on the UI. This class
+ * handles converting that data to drawing information for display. For example, the
+ * current powerup has to be drawn in a specific place, and will flash in and out when
+ * the powerup is fading (TODO note that not all game engine aspects are implemented
+ * yet).
+ * 
+ */
+public final class GameUIController {
+
+	
+	
+}
