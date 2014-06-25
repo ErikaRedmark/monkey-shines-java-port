5b5a4e7f9da2c26d165478e6d9e1c9cb57391361
Erika Redmark <maniacmonkeyinspace@gmail.com>
2014-06-24 19:05:08 -0700

    Having considerable difficulty getting the rows of background buttons to actually scroll; burned through multiple different layout managers and none seem to properly communicate to the ScrollPane the size of the panel when the buttons have been added.

--- Monkey Shines/src/org/erikaredmark/monkeyshines/editor/dialog/SetBackgroundDialog.java
+++ Monkey Shines/src/org/erikaredmark/monkeyshines/editor/dialog/SetBackgroundDialog.java
@@ -1,10 +1,15 @@
 package org.erikaredmark.monkeyshines.editor.dialog;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,6 +23,8 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SpringLayout;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -26,6 +33,7 @@ import org.erikaredmark.monkeyshines.background.FullBackground;
 import org.erikaredmark.monkeyshines.background.SingleColorBackground;
 import org.erikaredmark.monkeyshines.editor.resource.EditorResource;
 import org.erikaredmark.monkeyshines.resource.WorldResource;
+import org.erikaredmark.util.swing.layout.WrapLayout;
 
 public class SetBackgroundDialog extends JDialog {
 	private static final long serialVersionUID = 1L;
@@ -42,45 +50,23 @@ public class SetBackgroundDialog extends JDialog {
 		// Show a 'selected background' to the right so they know which of the three tabs
 		// is active and which background was picked.
 		setTitle("Background Picker");
-		setLayout(new FlowLayout() );
+		
+		final SpringLayout springLayout = new SpringLayout();
+		setLayout(springLayout);
 		
 		/* --------------------- Full background Panel  --------------------- */
-		JScrollPane fullBackgroundScrollPane = new JScrollPane();
 		JPanel fullBackgroundPanel = new JPanel();
-		fullBackgroundPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0) );
-		for (int i = 0; i < rsrc.getBackgroundCount(); i++) {
-			final Background next = rsrc.getBackground(i);
-			BufferedImage image = EditorResource.generateThumbnailForBackground((FullBackground)next);
-			JButton button = new JButton(new ImageIcon(image) );
-			button.setMargin(new Insets(2, 2, 2, 2) );
-			button.addActionListener(new ActionListener() {
-				@Override public void actionPerformed(ActionEvent e) {
-					setSelectedBackground(next);
-				}
-			});
-		}
-		fullBackgroundScrollPane.add(fullBackgroundPanel);
+		createThumbnailSpan(fullBackgroundPanel, rsrc, true);
+		JScrollPane fullBackgroundScrollPane = new JScrollPane(fullBackgroundPanel);
+		fullBackgroundScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		/* ------------------------- Pattern Panel  ------------------------- */
-		JScrollPane patternScrollPane = new JScrollPane();
 		JPanel patternPanel = new JPanel();
-		patternPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0) );
-		for (int i = 0; i < rsrc.getPatternCount(); i++) {
-			final Background next = rsrc.getPattern(i);
-			BufferedImage image = EditorResource.generateThumbnailForBackground((FullBackground)next);
-			JButton button = new JButton(new ImageIcon(image) );
-			button.setMargin(new Insets(2, 2, 2, 2) );
-			button.addActionListener(new ActionListener() {
-				@Override public void actionPerformed(ActionEvent e) {
-					setSelectedBackground(next);
-				}
-			});
-		}
-		patternScrollPane.add(patternPanel);
+		createThumbnailSpan(patternPanel, rsrc, false);
+		JScrollPane patternScrollPane = new JScrollPane(patternPanel);
+		patternScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		/* ----------------------------- Colour  ---------------------------- */
-		JPanel singleColorPanel = new JPanel();
-		singleColorPanel.setLayout(new FlowLayout() );
 		// If the background is initially a color background, use that as first color. Otherwise default black
 		Color initialColor =   currentBackground instanceof SingleColorBackground
 						     ? ((SingleColorBackground)currentBackground).getColor()
@@ -89,27 +75,38 @@ public class SetBackgroundDialog extends JDialog {
 		colorChooser.getSelectionModel().addChangeListener(new ChangeListener() {
 			@Override public void stateChanged(ChangeEvent event) {
 				Color newColor = colorChooser.getColor();
-				selectedBackground = new SingleColorBackground(newColor);
+				setSelectedBackground(new SingleColorBackground(newColor) );
 			}
 		});
-		singleColorPanel.add(colorChooser);
+		
+		JScrollPane singleColorPane = new JScrollPane(colorChooser);
 		
 		JTabbedPane differentBackgroundsPane = new JTabbedPane();
 		differentBackgroundsPane.addTab("Full", fullBackgroundPanel);
 		differentBackgroundsPane.addTab("Pattern", patternPanel);
-		differentBackgroundsPane.addTab("Solid", singleColorPanel);
-		add(differentBackgroundsPane);
+		differentBackgroundsPane.addTab("Solid", singleColorPane);
+		differentBackgroundsPane.setPreferredSize(new Dimension(560, 200) );
+		
+
 		
 		/* ---------------------------- Viewer ------------------------------ */
 		backgroundViewer = new Canvas() {
 			private static final long serialVersionUID = 1L;
 
 			@Override public void paint(Graphics g) {
-				selectedBackground.draw((Graphics2D)g);
+				if (selectedBackground instanceof FullBackground) {
+					BufferedImage thumbnail = EditorResource.generateThumbnailForBackground((FullBackground)selectedBackground);
+					Graphics2D g2d = (Graphics2D) g;
+					
+					g2d.drawImage(thumbnail, 0, 0, 160, 100, 0, 0, 160, 100, null);
+				} else {
+					// Solid colour; technically this only draws a portion of it, but that doesn't matter.
+					selectedBackground.draw((Graphics2D)g);
+				}
 			}
 		};
-		backgroundViewer.setSize(100, 160);
-		add(backgroundViewer);
+		backgroundViewer.setSize(160, 100);
+
 		
 		/* ------------------------ Okay and Cancel -------------------------- */
 		JButton okay = new JButton(new AbstractAction("Okay") {
@@ -119,7 +116,7 @@ public class SetBackgroundDialog extends JDialog {
 			}
 		});
 
-		add(okay);
+
 		
 		JButton cancel = new JButton(new AbstractAction("Cancel") {
 			private static final long serialVersionUID = 1L;
@@ -130,9 +127,60 @@ public class SetBackgroundDialog extends JDialog {
 			}
 		});
 		
-		add(cancel);
 		
-		setSize(640, 400);
+		/* --------------- Layout Components -------------- */
+		add(differentBackgroundsPane);
+		springLayout.putConstraint(SpringLayout.WEST, backgroundViewer, 10, SpringLayout.EAST, differentBackgroundsPane);
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, backgroundViewer, 0, SpringLayout.VERTICAL_CENTER, differentBackgroundsPane);
+		add(backgroundViewer, BorderLayout.EAST);
+		springLayout.putConstraint(SpringLayout.NORTH, okay, 10, SpringLayout.SOUTH, differentBackgroundsPane);
+		add(okay, BorderLayout.SOUTH);
+		springLayout.putConstraint(SpringLayout.WEST, cancel, 4, SpringLayout.EAST, okay);
+		springLayout.putConstraint(SpringLayout.NORTH, cancel, 0, SpringLayout.NORTH, okay);
+		add(cancel, BorderLayout.SOUTH);
+		
+		setSize(800, 400);
+	}
+	
+	/**
+	 * 
+	 * Adds to the given panel buttons with thumbnails all generated from the given 
+	 * list of backgrounds, along with having each one set the background to
+	 * the chosen one.
+	 * 
+	 * @param fullOrPattern
+	 * 		{@code true} to create a span for full backgrounds, {@code false} for patterns
+	 * 
+	 */
+	private void createThumbnailSpan(JPanel panel, WorldResource rsrc, boolean fullOrPattern) {
+		panel.setLayout(new WrapLayout(FlowLayout.LEFT, 0, 0) );
+		int count =   fullOrPattern
+					? rsrc.getBackgroundCount()
+				    : rsrc.getPatternCount();
+					
+		for (int i = 0; i < count; i++) {
+			final Background next =   fullOrPattern 
+									? rsrc.getBackground(i)
+									: rsrc.getPattern(i);
+									
+			BufferedImage image = EditorResource.generateThumbnailForBackground((FullBackground)next);
+			JButton button = new JButton(new ImageIcon(image) );
+			button.setMargin(new Insets(2, 2, 2, 2) );
+			button.addActionListener(new ActionListener() {
+				@Override public void actionPerformed(ActionEvent e) {
+					setSelectedBackground(next);
+				}
+			});
+			// Set the location in the grid based on iteration
+//			GridBagConstraints gbc = new GridBagConstraints();
+//			gbc.gridx = i % 3;
+//			gbc.gridy = i / 3;
+			panel.add(button);
+		}
+		
+		// Height will be dynamically calculated
+		panel.setSize(560, 1);
+
 	}
 	
 	/**
